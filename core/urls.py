7f5dcbb521b7dba3c781f92ed0d8fe6c09edc82a
Mohamed Mehfoud Bouh <medm.bouh@gmail.com>
2023-02-18 17:44:03 +0900

    first push

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
 from rest_framework import permissions
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
@@ -27,3 +29,5 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
